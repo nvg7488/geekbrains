54e4df107cc1a2419557993c8ac796df45be051a
nvg7488 <govoruchin_nv@mail.ru>
2019-04-07 13:39:48 +0700

    level 1, home work 4

--- level_001/src/main/java/ru/geekbrains/HomeWork4.java
+++ level_001/src/main/java/ru/geekbrains/HomeWork4.java
@@ -4,15 +4,15 @@ package ru.geekbrains;
  */
 import java.util.*;
 public class HomeWork4 {
-    private static int SIZE = 5;
-    private static int DOTS = 4;
-    private static char map [][] = new char [SIZE][SIZE];
-    private static Scanner sc = new Scanner (System.in);
-    private static Random rand = new Random ();
+    private static int size = 5;
+    private static int dots = 4;
+    private static char[][] map = new char[size][size];
+    private static Scanner sc = new Scanner(System.in);
+    private static Random rand = new Random();
 
     public static void main(String[] args) {
-        for (int x = 0; x < SIZE; x++) {
-            for (int y = 0; y < SIZE; y++) {
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
                 map[x][y] = '*';
             }
         }
@@ -35,20 +35,20 @@ public class HomeWork4 {
     }
 
     private static void printPole() {
-        for (int i = 0; i <= SIZE; i++) {
+        for (int i = 0; i <= size; i++) {
             System.out.print(i + " ");
         }
         System.out.println();
-        for (int x = 0; x < SIZE; x++) {
-            System.out.print((x+1) + " ");
-            for (int y = 0; y < SIZE; y++) {
+        for (int x = 0; x < size; x++) {
+            System.out.print((x + 1) + " ");
+            for (int y = 0; y < size; y++) {
                 System.out.print(map[x][y] + " ");
             }
             System.out.println();
         }
         boolean bln = true;
-        for (int x = 0; x < SIZE; x++) {
-            for (int y = 0; y < SIZE; y++) {
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
                 if (map[x][y] == '*') {
                     bln = false;
                 }
@@ -61,25 +61,26 @@ public class HomeWork4 {
     private static void stepRnd() {
         int x, y;
         do {
-            x = rand.nextInt(SIZE);
-            y = rand.nextInt(SIZE);
-        } while (!XY(x, y));
+            x = rand.nextInt(size);
+            y = rand.nextInt(size);
+        } while (!xy(x, y));
         map [x][y] = 'O';
-        System.out.println("Программа походила: " + (x+1) + "x" + (y+1));
+        System.out.println("Программа походила: " + (x + 1) + "x" + (y + 1));
     }
-    private static void stepMen() { int x, y;
+    private static void stepMen() {
+        int x, y;
         do {
             System.out.println("\nХод игрока:");
             System.out.print("\tX: ");
             x = sc.nextInt() - 1;
             System.out.print("\tY: ");
             y = sc.nextInt() - 1;
-        } while (!XY(x, y));
+        } while (!xy(x, y));
         map [x][y] = 'X';
     }
-    private static boolean XY(int x, int y) {
+    private static boolean xy(int x, int y) {
         boolean result;
-        if (x < 0 || x > SIZE || y < 0 || y > SIZE)	{
+        if (x < 0 || x > size || y < 0 || y > size)	{
             result = false;
         } else if (map [x][y] == '*') {
             result = true;
@@ -90,9 +91,9 @@ public class HomeWork4 {
     }
     private static boolean check(char ch) {
         boolean result = false;
-        int x, y, z = SIZE - DOTS;
+        int x, y, z = size - dots;
         for (x = 0; x < 2; x++) {
-            for (y = 0; y < SIZE; y++) {
+            for (y = 0; y < size; y++) {
                 if (blnDots(x, y, 1, 0, ch)) {
                     result = true;
                     break;
@@ -100,7 +101,7 @@ public class HomeWork4 {
             }
         }
         for (y = 0; y < 2; y++) {
-            for (x = 0; x < SIZE; x++) {
+            for (x = 0; x < size; x++) {
                 if (blnDots(x, y, 0, 1, ch)) {
                     result = true;
                     break;
@@ -115,7 +116,7 @@ public class HomeWork4 {
                 }
             }
         }
-        for (x = DOTS - 1; x < SIZE; x++) {
+        for (x = dots - 1; x < size; x++) {
             for (y = 0; y <= z; y++) {
                 if (dgnDots(x, y, -1, ch)) {
                     result = true;
@@ -126,17 +127,17 @@ public class HomeWork4 {
         return result;
     }
     private static boolean blnDots(int x, int y, int dx, int dy, char ch) {
-        int i, j, X, Y;
+        int i, j, xd, yd;
         boolean bln = true;
         if (dx == 0) {
-            X = x;
+            xd = x;
         } else {
-            X = x + DOTS;
+            xd = x + dots;
         }
         if (dy == 0) {
-            Y = y;
+            yd = y;
         } else {
-            Y = y + DOTS;
+            yd = y + dots;
         }
         i = x; do {
             j = y; do {
@@ -146,14 +147,14 @@ public class HomeWork4 {
                 x += dx;
                 y += dy;
                 j++;
-            } while (j < Y);
+            } while (j < yd);
             i++;
-        } while (i < X);
+        } while (i < xd);
         return bln;
     }
     private static boolean dgnDots(int x, int y, int dx, char ch) {
         boolean result = true;
-        int z = y + DOTS;
+        int z = y + dots;
         for (; y < z; y++) {
             if (map [x][y] != ch) {
                 result = false;
