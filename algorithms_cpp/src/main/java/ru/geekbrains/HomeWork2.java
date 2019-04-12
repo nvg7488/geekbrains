2abcb4d7e3e27d0bb9cfaca2f8b69369ffcb4fa5
nvg7488 <govoruchin_nv@mail.ru>
2019-04-12 18:40:50 +0700

    algorithms cpp, home work 2, test 1

--- algorithms_cpp/src/main/java/ru/geekbrains/HomeWork2.java
+++ algorithms_cpp/src/main/java/ru/geekbrains/HomeWork2.java
@@ -2,17 +2,18 @@ package ru.geekbrains;
 /**
  * @author Николай Говорухин (govoruchin_nv@mail.ru)
  */
+import java.util.LinkedList;
 public class HomeWork2 {
+    private LinkedList binar = new LinkedList();
     /**
      * Реализовать функцию перевода из 10 системы в двоичную используя рекурсию.
      * @param desc
      * @return String binar
      */
-    public String task1(int desc) {
-        String binar = null;
-        if (desc > 0) {
-            binar.concat(String.valueOf(desc % 2));
-            binar.concat(task1(desc / 2));
+    public LinkedList task1(int desc) {
+        binar.add(desc % 2);
+        if (desc / 2 > 0) {
+            task1(desc / 2);
         }
         return binar;
     }
--- algorithms_cpp/src/test/java/ru/geekbrains/TestHomeWork2.java
+++ algorithms_cpp/src/test/java/ru/geekbrains/TestHomeWork2.java
@@ -2,13 +2,57 @@ package ru.geekbrains;
 /**
  * @author Николай Говорухин (govoruchin_nv@mail.ru)
  */
+import java.util.LinkedList;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 public class TestHomeWork2 {
     @Test
-    public void testTask1() {
+    public void testTask1desc0() {
         HomeWork2 homework = new HomeWork2();
-        assertThat(homework.task1(1), is("1"));
+        LinkedList result = new LinkedList();
+        result.add(0);
+        assertThat(homework.task1(0), is(result));
+    }
+    @Test
+    public void testTask1desc1() {
+        HomeWork2 homework = new HomeWork2();
+        LinkedList result = new LinkedList();
+        result.add(1);
+        assertThat(homework.task1(1), is(result));
+    }
+    @Test
+    public void testTask1desc2() {
+        HomeWork2 homework = new HomeWork2();
+        LinkedList result = new LinkedList();
+        result.add(0);
+        result.add(1);
+        assertThat(homework.task1(2), is(result));
+    }
+    @Test
+    public void testTask1desc3() {
+        HomeWork2 homework = new HomeWork2();
+        LinkedList result = new LinkedList();
+        result.add(1);
+        result.add(1);
+        assertThat(homework.task1(3), is(result));
+    }
+    @Test
+    public void testTask1desc4() {
+        HomeWork2 homework = new HomeWork2();
+        LinkedList result = new LinkedList();
+        result.add(0);
+        result.add(0);
+        result.add(1);
+        assertThat(homework.task1(4), is(result));
+    }
+    @Test
+    public void testTask1desc5() {
+        HomeWork2 homework = new HomeWork2();
+        LinkedList result = new LinkedList();
+        result.add(1);
+        result.add(0);
+        result.add(1);
+        assertThat(homework.task1(5), is(result));
     }
 }
