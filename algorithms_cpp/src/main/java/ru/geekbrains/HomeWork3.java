bc91a934764ffd00545a318b8286b341e45e7037
nvg7488 <govoruchin_nv@mail.ru>
2019-04-13 20:02:03 +0700

    algorithms cpp, home work 3

--- algorithms_cpp/src/main/java/ru/geekbrains/HomeWork3.java
+++ algorithms_cpp/src/main/java/ru/geekbrains/HomeWork3.java
@@ -1,12 +1,6 @@
 package ru.geekbrains;
 /**
  * @author Николай Говорухин (govoruchin_nv@mail.ru)
- *
- * 2*   Реализовать шейкерную сортировку.
- * 3    Реализовать бинарный алгоритм поиска в виде функции, которой передаётся отсортированный массив.
- *  Функция возвращает индекс найденного элемента, или -1 если элемент не найден.
- * 4*   Подсчитать количество операций для каждой из сортировок и сравнить его с асимптотической
- *  сложностьюалгоритма.
  */
 public class HomeWork3 {
     /**
@@ -26,5 +20,62 @@ public class HomeWork3 {
             }
         }
         return array;
+    }
+
+    /**
+     * Реализовать шейкерную сортировку.
+     * @param array
+     * @return array
+     */
+    public int[] task2(int[] array) {
+        int tmp;
+        int left = 1;
+        int right = array.length - 1;
+        while (left <= right) {
+            for (int index = right; index >= left; index--) {
+                if (array[index - 1] > array[index]) {
+                    tmp = array[index - 1];
+                    array[index - 1] = array[index];
+                    array[index] = tmp;
+                }
+            }
+            left++;
+            for (int index = left; index <= right; index++) {
+                if (array[index - 1] > array[index]) {
+                    tmp = array[index - 1];
+                    array[index - 1] = array[index];
+                    array[index] = tmp;
+                }
+            }
+            right--;
+        }
+        return array;
+    }
+
+    /**
+     * Реализовать бинарный алгоритм поиска в виде функции, которой передаётся отсортированный массив.
+     * Функция возвращает индекс найденного элемента, или -1 если элемент не найден.
+     * @param array
+     * @return index
+     */
+    public int task3(int[] array, int from, int to, int vlm) {
+        int result = -1;
+        if (to - from == 1) {
+            if (array[from] == vlm) {
+                result = vlm;
+            }
+            if (array[to] == vlm) {
+                result = vlm;
+            }
+        } else {
+            int middle = (from + to) / 2;
+            if (array[middle] == vlm) {
+                result = middle;
+            }
+            if (array[middle] < vlm) {
+                result = task3(array, middle, to, vlm);
+            }
+        }
+        return result;
     }
 }
--- algorithms_cpp/src/test/java/ru/geekbrains/TestHomeWork3.java
+++ algorithms_cpp/src/test/java/ru/geekbrains/TestHomeWork3.java
@@ -13,4 +13,18 @@ public class TestHomeWork3 {
         int[] res = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         assertThat(homework.task1(arr), is(res));
     }
+    @Test
+    public void testTask2() {
+        HomeWork3 homework = new HomeWork3();
+        int[] arr = {1, 4, 7, 0, 2, 5, 8, 3, 6, 9};
+        int[] res = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        assertThat(homework.task2(arr), is(res));
+    }
+    @Test
+    public void testTask3() {
+        HomeWork3 homework = new HomeWork3();
+        int[] arr = {1, 4, 7, 0, 2, 5, 8, 3, 6, 9};
+        assertThat(homework.task3(arr, 0, arr.length - 1, 9), is(9));
+        assertThat(homework.task3(arr, 0, arr.length - 1, 13), is(-1));
+    }
 }
